5be6681052575c0b43672fcfd97a8b727cf5af07
weicaiteo <weicaiteo@gmail.com>
2016-04-12 15:21:25 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -83,7 +83,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl, 
-  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML,
+  [...], // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
@@ -97,14 +98,12 @@ function buildAndShowHomeHTML (categories) {
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
-      document.querySelector("#main-content").innerHTML = homeHtml;
-      
+
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       // var chosenCategoryShortName = ....
       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-      console.log('chosenCategoryShortName: ' + chosenCategoryShortName);
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
@@ -121,11 +120,19 @@ function buildAndShowHomeHTML (categories) {
       var homeHtmlToInsertIntoMainPage = insertProperty (homeHtml, "randomCategoryShortName", "'" + chosenCategoryShortName + "'");
       console.log('homeHtmlToInsertIntoMainPage: ');
       console.log(homeHtmlToInsertIntoMainPage);
+      //   var finalHtml = categoriesViewHtml;
+    //   finalHtml += "<section class='row'>";
+      
+      
 
-      // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
+
+
+
+      // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that. 
       // ....
+      
       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
@@ -345,3 +352,4 @@ function insertItemPortionName(html,
 global.$dc = dc;
 
 })(window);
+
